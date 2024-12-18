839a65a0c01d51a968fdb7071aa2d4d3ab953208
Sayana Surendran <sayanasurendran31@gmail.com>
2024-12-18 09:53:21 +0100

    created model and database connection utilty

---
+++ src/main/java/se/lexicon/model/City.java
@@ -0,0 +1,83 @@
+package se.lexicon.model;
+
+import java.util.Objects;
+
+public class City {
+
+    private int id;
+    private String name;
+    private String countryCode;
+    private String district;
+    private int population;
+
+    public City(String name, String countryCode, String district, int population) {
+        this.name = name;
+        this.countryCode = countryCode;
+        this.district = district;
+        this.population = population;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        City city = (City) o;
+        return getId() == city.getId() && getPopulation() == city.getPopulation() && Objects.equals(getName(), city.getName()) && Objects.equals(getCountryCode(), city.getCountryCode()) && Objects.equals(getDistrict(), city.getDistrict());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getCountryCode(), getDistrict(), getPopulation());
+    }
+
+    @Override
+    public String toString() {
+        return "City{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", countryCode='" + countryCode + '\'' +
+                ", district='" + district + '\'' +
+                ", population=" + population +
+                '}';
+    }
+}
