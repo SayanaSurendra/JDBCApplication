0b3a1dc6633cc7b0b96ce251caaea17c1711fe01
Sayana Surendran <sayanasurendran31@gmail.com>
2024-12-18 13:09:21 +0100

    updated the interface with one more method (findAll())

--- src/main/java/se/lexicon/dao/CityDao.java
+++ src/main/java/se/lexicon/dao/CityDao.java
@@ -9,6 +9,7 @@ public interface CityDao {
     City findById(int id);
     List<City> findByCode(String code);
     List<City> findByCodeName(String name);
+    List<City> findAll();
     City add(City city);
     City update(City city);
     int delete(City city);
